17cbd0c6d06db3e59bbfa6b37332d1447dcc0e88
Valentim Helio <valentimhelio@gmail.com>
2025-01-15 16:19:20 -0400

    80. 0806 Iniciando a Implementação do Repositório de Produtos

--- GeekShopping.ProductAPI/Repository/IProductRepository.cs
+++ GeekShopping.ProductAPI/Repository/IProductRepository.cs
@@ -5,7 +5,7 @@ namespace GeekShopping.ProductAPI.Repository
     public interface IProductRepository
     {
         Task<IEnumerable<ProductVO>> FindAll();
-        Task<ProductVO> FindId(long id);
+        Task<ProductVO> FindById(long id);
         Task<ProductVO> Create(ProductVO product);
         Task<ProductVO> Update(ProductVO product);
         Task<bool> Delete(long id);
