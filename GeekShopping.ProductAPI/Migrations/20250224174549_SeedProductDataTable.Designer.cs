93d9ab39d9c82b3b0b7d104512aab4bffa3e2345
valentimhelio <valentimhelio@gmail.com>
2025-02-24 14:37:53 -0400

    85. 0811 Configurando o Seed Data no Microsserviço de Produtos 86. 0812 Executando as Seeds (Migrations) e Populando o Banco de Dados 87. 0813 [UPDATES] O que vai Mudar no seu Projeto com .NET 6 LTS

--- GeekShopping.ProductAPI/Migrations/20250224174549_SeedProductDataTable.Designer.cs
+++ GeekShopping.ProductAPI/Migrations/20250224174549_SeedProductDataTable.Designer.cs
@@ -3,6 +3,7 @@ using GeekShopping.ProductAPI.Model.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace GeekShopping.ProductAPI.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250224174549_SeedProductDataTable")]
+    partial class SeedProductDataTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
