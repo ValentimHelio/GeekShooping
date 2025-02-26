dea7e14d23fe9483d5368897a10f152a28efa34a
valentimhelio <valentimhelio@gmail.com>
2025-02-26 14:13:43 -0400

    98. 1003 Implementando o Controller de Produtos 99. 1004 Implementando a Tela de Listagem de Produtos 100. 1005 Adicionando os Botões de Update e Delete

--- GeekShooping.Web/Program.cs
+++ GeekShooping.Web/Program.cs
@@ -4,7 +4,7 @@ using GeekShooping.Web.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddHttpClient<IProductService, ProductService>(
-    c => c.BaseAddress = new Uri(builder.Configuration["ServiveUrls:ProductAPI"])
+    c => c.BaseAddress = new Uri(builder.Configuration["ServicesUrls:ProductAPI"])
 );
 
 
